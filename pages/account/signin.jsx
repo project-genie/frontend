1b0b06f904d926783ea66013fbee59a9e3a43044
mert18 <mertuygur02@gmail.com>
2023-03-23 16:29:31 +0300

    add(auth): :curling_stone: sign in page added.

--- pages/account/signin.jsx
+++ pages/account/signin.jsx
@@ -5,10 +5,13 @@ import * as yup from "yup";
 import { NextResponse } from "next/server";
 import axios from "axios";
 import Router, { useRouter } from "next/router";
+import Image from "next/image";
+import Spinner from "../components/Spinner";
 
 const SignIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
 
   const isAuthenticated = async () => {
@@ -31,12 +34,12 @@ const SignIn = () => {
 
   const formik = useFormik({
     initialValues: {
-      email: "mertuygur02@gmail.com",
-      password: "Hachiko2k.pr",
+      email: "",
+      password: "",
     },
     onSubmit: async (values) => {
-      console.log("values", values);
       try {
+        setLoading(true);
         const response = await axios.post(
           "http://localhost:8080/api/auth/signin",
           values,
@@ -44,40 +47,87 @@ const SignIn = () => {
             withCredentials: true,
           }
         );
-        console.log("Response: ", response);
+        setLoading(false);
         router.push("/organizations");
       } catch (error) {
+        setLoading(false);
         console.log("Error occured!: ", error);
       }
     },
   });
 
   return (
-    <div className="grid grid-cols-12 min-h-screen">
-      <div className="col-start-1 col-end-4">
-        <form onSubmit={formik.handleSubmit}>
-          <label htmlFor="email">Email Address</label>
-          <input
-            id="email"
-            name="email"
-            type="email"
-            onChange={formik.handleChange}
-            value={formik.values.email}
+    <div className="grid grid-cols-6 grid-rows-6 min-h-screen">
+      <div className="grid lg:col-start-1 lg:col-end-3 lg:row-start-1 lg:row-end-7 row-start-1 row-end-4 col-start-1 col-end-7 py-5 bg-secondary-100">
+        <div className="row-start-1 row-end-3 flex justify-center items-center">
+          <Image
+            src="/logo/logo_dark.svg"
+            width={200}
+            height={200}
+            alt="Project Genie Logo"
           />
+        </div>
+        <div className="row-start-3 row-end-6 flex justify-center">
+          <div className="w-[80%] block">
+            <div className="mb-6">
+              <h2 className="font-medium text-lg">Log in to your account.</h2>
+            </div>
 
-          <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            name="password"
-            type="password"
-            onChange={formik.handleChange}
-            value={formik.values.password}
-          />
+            <form onSubmit={formik.handleSubmit}>
+              <div class="mb-6">
+                <label
+                  for="email"
+                  className="block mb-1 text-sm font-medium text-neutral-800"
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  className="bg-transparent border border-neutral-800 text-neutral-800 text-sm rounded-lg  focus:ring-primary-500 focus:border-primary-500 outline-primary-500 block w-full p-2.5 "
+                  placeholder="example@provider.com"
+                  onChange={formik.handleChange}
+                  value={formik.values.email}
+                  required
+                />
+              </div>
+              <div className="mb-6">
+                <label
+                  for="password"
+                  className="block mb-1 text-sm font-medium text-neutral-800"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  className="bg-transparent border border-neutral-800 text-neutral-800 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 outline-primary-500 block w-full p-2.5 "
+                  onChange={formik.handleChange}
+                  value={formik.values.password}
+                  required
+                />
+              </div>
 
-          <button type="submit">Submit</button>
-        </form>
+              <button
+                type="submit"
+                className="text-neutral-50 bg-primary-500 hover:bg-primary-600 focus:ring-4 focus:outline-none focus:ring-primary-500 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+              >
+                {loading ? <Spinner /> : <p>Log In</p>}
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
-      <div className="col-start-4 col-end-13"></div>
+      <div className="bg-neutral-900 lg:col-start-3 lg:col-end-7 lg:row-start-1 lg:row-end-7 row-start-4 row-end-7 col-start-1 col-end-7">
+        <div className="flex justify-center items-center h-full">
+          <Image
+            src="/logo/logo_light.svg"
+            width={400}
+            height={400}
+            alt="Project Genie Logo"
+          />
+        </div>
+      </div>
     </div>
   );
 };
