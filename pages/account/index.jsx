42d35f80a1ed7f2b8b75676ad28fea98268b2a20
mert18 <mertuygur02@gmail.com>
2023-04-12 12:27:23 +0300

    add(account): :rice: account update page.

--- pages/account/index.jsx
+++ pages/account/index.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import MainLayout from "../components/layout/MainLayout";
+import AccountUpdateForm from "../components/AccountUpdateForm";
 
 const Account = () => {
-  return <MainLayout>Account</MainLayout>;
+  return (
+    <MainLayout>
+      {" "}
+      <div className="flex flex-col justify-center items-center">
+        <div className="flex justify-center items-center mt-10">
+          <p className="text-base">Edit the account.</p>
+        </div>
+        <div className="w-[80%] bg-secondary-50 rounded-lg mt-10">
+          <AccountUpdateForm />
+        </div>
+      </div>
+    </MainLayout>
+  );
 };
 
 export default Account;
